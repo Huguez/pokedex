6d61d46d5dfe07e2fb15d037d0443cb47a9c3d97
Carlos Huguez <carlos.huguez@sonorasoft.com>
2025-06-20 11:47:40 -0700

    react native paper

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import { WrapSafe } from './components';
 import { StackNavigation } from './navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 function App() {
  
   return (
-    <SafeAreaProvider>
-      <StatusBar
-          backgroundColor="#61dafb"
-          barStyle={ 'dark-content' }
-      />
-      <WrapSafe>
-        <StackNavigation />
-      </WrapSafe>
-    </SafeAreaProvider>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <StatusBar backgroundColor="#61dafb" barStyle={ 'dark-content' } />
+        <WrapSafe>
+          <StackNavigation />
+        </WrapSafe>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,27 @@
 
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { NavHook } from '../infrastructure'
+import { Button } from 'react-native-paper'
 
 export const HomeScreen = () => {
    const navigation = useNavigation<NavHook>()
    return (
       <View>
          <Text>HomeScreen</Text>
+
+         <View style={{ marginVertical: 15 }}></View>
          
-         <Pressable onPress={()=> navigation.navigate('Pokedex') }>
-            <Text>PokedexScreen</Text>
-         </Pressable>
+         <Button mode="contained" onPress={()=> navigation.navigate('Pokedex') }>
+            PokedexScreen
+         </Button>
+         
+         <View style={{ marginVertical: 15 }}></View>
 
-         <Pressable onPress={()=> navigation.navigate('Search') }>
-            <Text>SearchScreen</Text>
-         </Pressable>
-
+         <Button mode="contained" onPress={()=> navigation.navigate('Search') }>
+            SearchScreen
+         </Button>
       </View>
    )
 }
